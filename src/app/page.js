1ec98bca2959bfca4f966a564ca5aaef0e57725a
axylf <axylofredrick@gmail.com>
2025-03-07 22:45:52 -0500

    fix homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 export default function Home() {
     return (
         <>
             <NavBar />
-            <main className="flex flex-col items-center justify-center min-h-screen">
-                <h1 className="text-4xl font-bold mb-4">Welcome to ApexFitness</h1>
+            <div className="flex flex-col items-center justify-center min-h-screen">
+                <h2 className="text-4xl font-bold mb-4">Welcome to ApexFitness</h2>
                 <p className="text-lg text-gray-700">Track workouts, log progress, and stay fit!</p>
-            </main>
+            </div>
+            <Footer/>
         </>
 
     );
