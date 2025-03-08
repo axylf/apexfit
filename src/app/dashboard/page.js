f86f289a4b607bba0b3ff4f6b13585a4c7d48848
Hazera Sarker <131634490+hazelcodes54@users.noreply.github.com>
2025-03-08 00:11:24 -0500

    Update page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -6,14 +6,11 @@ export default function Dashboard() {
     return (
         <div className={styles.dashboardContainer}>
             <NavBar />
-            <nav className={styles.navLinks}>
+            <div className={styles.navLinks}>
                 <Link href="/dashboard">Home</Link>
                 <Link href="/dashboard/diet">Diet</Link>
                 <Link href="/dashboard/workout">Workout</Link>
-            </nav>
-            <main className={styles.mainContent}>
-                <h1> Have fun designing this Hazerakjshd :D </h1>
-            </main>
+            </div>
         </div>
     );
 }
